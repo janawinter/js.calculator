871e6b4656d3396b86e3069f80281289868c4e9c
Jana Winter <janawhitta@gmail.com>
2016-07-08 20:16:07 +1200

    updated

--- calculator.js
+++ calculator.js
@@ -1,14 +1,3 @@
-/*$(document).ready(function(){
-  var testNumLength = function(number) {
-        if (number.length > 9) {
-            totaldiv.text(number.substr(number.length-9,9));
-            if (number.length > 15) {
-                number = "";
-                totaldiv.text("Err");
-            }
-        }
-    };
-*/
 
 $(document).ready(function(){
     var testNumLength = function(number) {
@@ -26,39 +15,39 @@ $(document).ready(function(){
       var totaldiv = $("#total");
       totaldiv.text("0");
 
-      $(".number").click(function(){
+      $(".number").click(function() {
       number += $(this).text();
       totaldiv.val(number);
       testNumLength(number);
       });
-      $(".operator").click(function(){
+      $(".operator").click(function() {
       operator = $(this).text();
       newnumber = number;
       number = "";
       //totaldiv.val("");
       });
-      $(".clear,.allclear").click(function(){
+      $(".clear,.allclear").click(function() {
       number = "";
       totaldiv.val("0");
-      if ($(this).hasClass("allclear")){
+      if ($(this).hasClass("allclear")) {
         newnumber = "";
       }
       });
 
- $(".equals").click(function(){
+ $(".equals").click(function() {
     if (operator === "+") {
       result=parseInt(newnumber, 10) + parseInt(number, 10);
       number = result.toString(10);
 
-    } else if (operator === "-"){
+    } else if (operator === "-") {
    result=parseInt(newnumber, 10) -  parseInt(number, 10);
 
       number = result.toString(10);
- } else if (operator === "÷"){
+    } else if (operator === "÷") {
    result=parseInt(newnumber, 10) / parseInt(number, 10);
       number = result.toString(10);
- }
-  else if (operator === "X"){
+    }
+     else if (operator === "X") {
  result=parseInt(newnumber, 10) * parseInt(number, 10);
       number = result.toString(10);
  }
